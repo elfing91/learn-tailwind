2bcc7dd15d06fdc11bfee94fa83170489d1f7ef4
민정 <minjaung5073@naver.com>
2024-09-01 21:27:12 +0900

    글자모양 속성 extends내부에 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,14 @@ module.exports = {
     extend: {
       fontSize: {
         xxs: '0.625rem',
-        base: '1rem',
-        xl: '1.25rem',
-        '2xl': '1.563rem',
-        '3xl': '1.953rem',
-        '4xl': '2.441rem',
-        '5xl': '3.052rem',
-      }
+       
+        
+      },
+      fontFamily: {
+       pre:[ "Pretendard Variable", Pretendard],
+      },
     },
   },
   plugins: [],
-}
+};
 
