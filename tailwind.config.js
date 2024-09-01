9ffa44d793e58aec8c1e8b3942c204dfd5a7eb50
민정 <minjaung5073@naver.com>
2024-09-01 20:24:24 +0900

    타이포그라피 파일 생성/글자크기 속성 extends내부에 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge:["./src/**/*.html"],
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: '0.625rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      }
+    },
   },
   plugins: [],
 }
